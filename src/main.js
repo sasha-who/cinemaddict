54f1cb08369fcf34c583095421b8e028e47afcce
Sasha <warmsasha@gmail.com>
2020-04-02 11:41:42 +0400

    Добавляет функцию render и отрисовывает компоненты на странице

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const FILM_COUNT = 5;
+const EXTRA_FILM_COUNT = 2;
+
 const createProfileTemplate = () => {
   return (
     `<section class="header__profile profile">
@@ -30,56 +33,6 @@ const createSortTemplate = () => {
       <li><a href="#" class="sort__button">Sort by date</a></li>
       <li><a href="#" class="sort__button">Sort by rating</a></li>
     </ul>`
-  );
-};
-
-const createUserStatisticTemplate = () => {
-  return (
-    `<section class="statistic">
-      <p class="statistic__rank">
-        Your rank
-        <img class="statistic__img" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
-        <span class="statistic__rank-label">Sci-Fighter</span>
-      </p>
-
-      <form action="https://echo.htmlacademy.ru/" method="get" class="statistic__filters">
-        <p class="statistic__filters-description">Show stats:</p>
-
-        <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-all-time" value="all-time" checked>
-        <label for="statistic-all-time" class="statistic__filters-label">All time</label>
-
-        <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-today" value="today">
-        <label for="statistic-today" class="statistic__filters-label">Today</label>
-
-        <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-week" value="week">
-        <label for="statistic-week" class="statistic__filters-label">Week</label>
-
-        <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-month" value="month">
-        <label for="statistic-month" class="statistic__filters-label">Month</label>
-
-        <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-year" value="year">
-        <label for="statistic-year" class="statistic__filters-label">Year</label>
-      </form>
-
-      <ul class="statistic__text-list">
-        <li class="statistic__text-item">
-          <h4 class="statistic__item-title">You watched</h4>
-          <p class="statistic__item-text">22 <span class="statistic__item-description">movies</span></p>
-        </li>
-        <li class="statistic__text-item">
-          <h4 class="statistic__item-title">Total duration</h4>
-          <p class="statistic__item-text">130 <span class="statistic__item-description">h</span> 22 <span class="statistic__item-description">m</span></p>
-        </li>
-        <li class="statistic__text-item">
-          <h4 class="statistic__item-title">Top genre</h4>
-          <p class="statistic__item-text">Sci-Fi</p>
-        </li>
-      </ul>
-
-      <div class="statistic__chart-wrap">
-        <canvas class="statistic__chart" width="1000"></canvas>
-      </div>
-    </section>`
   );
 };
 
@@ -145,7 +98,7 @@ const createShowMoreButtonTemplate = () => {
   );
 };
 
-const createFilmStatisticTemplate = () => {
+const createFilmsStatisticsTemplate = () => {
   return (
     `<p>130 291 movies inside</p>`
   );
@@ -324,3 +277,42 @@ const createFilmDetailsTemplate = () => {
     </section>`
   );
 };
+const render = (template, container, position = `beforeend`) => {
+  container.insertAdjacentHTML(position, template);
+};
+
+const headerElement = document.querySelector(`.header`);
+
+render(createProfileTemplate(), headerElement);
+
+const mainElement = document.querySelector(`.main`);
+
+render(createMainNavigationTemplate(), mainElement);
+render(createSortTemplate(), mainElement);
+render(createFilmsTemplate(), mainElement);
+
+const filmsElement = mainElement.querySelector(`.films`);
+const filmsListElement = filmsElement.querySelector(`.films-list`);
+const filmsContainerElement = filmsListElement.querySelector(`.films-list__container`);
+
+for (let i = 0; i < FILM_COUNT; i++) {
+  render(createFilmCardTemplate(), filmsContainerElement);
+}
+
+render(createShowMoreButtonTemplate(), filmsListElement);
+render(createTopRatedTemplate(), filmsElement);
+render(createMostCommentedTemplate(), filmsElement);
+
+const extraFilmsListElements = filmsElement.querySelectorAll(`.films-list--extra .films-list__container`);
+
+extraFilmsListElements.forEach((item) => {
+  for (let i = 0; i < EXTRA_FILM_COUNT; i++) {
+    render(createFilmCardTemplate(), item);
+  }
+});
+
+const footerElement = document.querySelector(`.footer`);
+const filmsStatisticsElement = footerElement.querySelector(`.footer__statistics`);
+
+render(createFilmsStatisticsTemplate(), filmsStatisticsElement);
+render(createFilmDetailsTemplate(), footerElement, `afterend`);
