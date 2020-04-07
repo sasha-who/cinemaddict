7bffd00f7d767f27d02b8f4891fa419808b7d530
Sasha <warmsasha@gmail.com>
2020-04-08 00:00:57 +0400

    Создаёт отдельные модули для компонентов и ипортирует их в main.js

---
+++ src/components/films-statistics.js
@@ -0,0 +1,3 @@
+export const createFilmsStatisticsTemplate = () => (
+  `<p>130 291 movies inside</p>`
+);
---
+++ src/components/top-rated.js
@@ -0,0 +1,7 @@
+export const createTopRatedTemplate = () => (
+  `<section class="films-list--extra">
+    <h2 class="films-list__title">Top rated</h2>
+
+    <div class="films-list__container"></div>
+  </section>`
+);
