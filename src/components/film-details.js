8c82ccdaa83d9d581aeb724fbfc4bff7398ba659
sasha-who <warmsasha@gmail.com>
2020-04-21 09:56:51 +0300

    Выносит функцию создания разметки комментариев ищ общей функции

--- src/components/film-details.js
+++ src/components/film-details.js
@@ -1,3 +1,27 @@
+const getCommentsMarkup = (commentsArray) => {
+  const commentMarkup = commentsArray.map((item) => {
+    const {content, emotion, author, date} = item;
+
+    return (
+      `<li class="film-details__comment">
+        <span class="film-details__comment-emoji">
+          <img src="./images/emoji/${emotion}.png" width="55" height="55" alt="emoji-${emotion}">
+        </span>
+        <div>
+          <p class="film-details__comment-text">${content}</p>
+          <p class="film-details__comment-info">
+            <span class="film-details__comment-author">${author}</span>
+            <span class="film-details__comment-day">${date}</span>
+            <button class="film-details__comment-delete">Delete</button>
+          </p>
+        </div>
+      </li>`
+    );
+  });
+
+  return commentMarkup.join(`\n`);
+};
+
 export const createFilmDetailsTemplate = (film) => {
   const {
     poster,
@@ -27,30 +51,6 @@ export const createFilmDetailsTemplate = (film) => {
     .join(`\n`);
 
   const checkControls = (flag) => flag ? `checked` : ``;
-
-  const getCommentsMarkup = (commentsArray) => {
-    const commentMarkup = commentsArray.map((item) => {
-      const {content, emotion, author, date} = item;
-
-      return (
-        `<li class="film-details__comment">
-          <span class="film-details__comment-emoji">
-            <img src="./images/emoji/${emotion}.png" width="55" height="55" alt="emoji-${emotion}">
-          </span>
-          <div>
-            <p class="film-details__comment-text">${content}</p>
-            <p class="film-details__comment-info">
-              <span class="film-details__comment-author">${author}</span>
-              <span class="film-details__comment-day">${date}</span>
-              <button class="film-details__comment-delete">Delete</button>
-            </p>
-          </div>
-        </li>`
-      );
-    });
-
-    return commentMarkup.join(`\n`);
-  };
 
   return (
     `<section class="film-details">
