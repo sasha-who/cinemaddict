7b9133c541ec832b0612160bdff3806d149f5504
sasha-who <warmsasha@gmail.com>
2020-04-28 22:04:54 +0300

    Добавляет функции для вставки и удаления дочернего DOM-элемента

--- src/render.js
+++ src/render.js
@@ -1,17 +1,27 @@
 import {RenderPosition} from "./const.js";
 
-export const render = (element, container, position = RenderPosition.BEFOREEND) => {
+export const render = (component, container, position = RenderPosition.BEFOREEND) => {
   switch (position) {
     case RenderPosition.BEFOREEND:
-      container.append(element);
+      container.append(component.getElement());
       break;
 
     case RenderPosition.AFTERBEGIN:
-      container.prepend(element);
+      container.prepend(component.getElement());
       break;
   }
 };
 
-export const remove = (element) => {
-  element.remove();
+export const remove = (component) => {
+  component.getElement().remove();
+  component.removeElement();
+};
+
+export const appendChild = (component, parent) => {
+  parent.appendChild(component.getElement());
+};
+
+export const removeChild = (component, parent) => {
+  parent.removeChild(component.getElement());
+  component.removeElement();
 };
