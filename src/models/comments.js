bfcc87a64a1ed0be468afda8d6ead3b766679260
sasha-who <warmsasha@gmail.com>
2020-05-07 10:11:53 +0300

    Добавляет в модель методы для удаления и создания комментария

--- src/models/comments.js
+++ src/models/comments.js
@@ -14,6 +14,25 @@ export default class Comments {
     this._callHandlers(this._dataChangeHandlers);
   }
 
+  removeComment(id) {
+    const index = this._comments.findIndex((it) => it.id === id);
+
+    if (index === -1) {
+      return false;
+    }
+
+    this._comments = [].concat(this._comments.slice(0, index), this._comments.slice(index + 1));
+
+    this._callHandlers(this._dataChangeHandlers);
+
+    return true;
+  }
+
+  addComment(comment) {
+    this._comments = [].concat(this._comments, comment);
+    this._callHandlers(this._dataChangeHandlers);
+  }
+
   setDataChangeHandler(handler) {
     this._dataChangeHandlers.push(handler);
   }
