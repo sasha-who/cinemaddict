36c59c79e9810dd45b56ead0575b43e44f5f6283
sasha-who <warmsasha@gmail.com>
2020-05-15 00:11:46 +0300

    Правит ошибку линтера

--- src/controllers/filter.js
+++ src/controllers/filter.js
@@ -1,5 +1,5 @@
 import {Filters, FilterType, RenderPosition} from "../const.js";
-import {replace, render, remove} from "../utils/render.js";
+import {replace, render} from "../utils/render.js";
 import {getFilmsByFilter} from "../utils/filter.js";
 import FilterComponent from "../components/filter.js";
 
